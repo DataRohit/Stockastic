139ab498be1150e34917c29bba9ecb1e36cceab1
DataRohit <rohit.vilas.ingole@gmail.com>
2023-08-27 01:27:35 +0530

    path to file updated

--- streamlit_app/helper.py
+++ streamlit_app/helper.py
@@ -16,7 +16,7 @@ from statsmodels.tsa.ar_model import AutoReg
 # Create function to fetch stock name and id
 def fetch_stocks():
     # Load the data
-    df = pd.read_csv(Path.cwd().parent / "data" / "equity_issuers.csv")
+    df = pd.read_csv(Path.cwd().parent.parent / "data" / "equity_issuers.csv")
 
     # Filter the data
     df = df[["Security Code", "Issuer Name"]]
